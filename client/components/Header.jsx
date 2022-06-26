b6e243d34601c1878e2a31bbd1965c3d32f8e25b
Chou Lee <Chou@chous-device.local>
2022-06-26 10:24:30 +0700

    add struct

---
+++ client/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { NextUIProvider } from '@nextui-org/react';
+
+const Header = () => {
+    return (
+        <header>
+            <div className="logo"></div>
+            <div className="categories"></div>
+            <div className="settings"></div>
+        </header>
+    )
+}
