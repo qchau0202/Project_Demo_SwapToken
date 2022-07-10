640ae3f3fc7067e93a9cd4127b5f4508833c52a2
nhad1702 <anhducnguyenhuu2@gmail.com>
2022-07-10 10:04:22 +0700

    .

--- client/src/pages/Swap.jsx
+++ client/src/pages/Swap.jsx
@@ -1,14 +1,38 @@
 import React from "react";
-import {  Card, Dropdown, Text } from "@nextui-org/react"
+import {  Avatar, Card, Dropdown, Grid, Text, Tooltip } from "@nextui-org/react"
 
 const Swap = () => {
   return [
     <div>
     <Card>
       <Text h4>Swap</Text>
-      <Dropdown disableAnimation>
-        
-      </Dropdown>
+      <Grid.Container justify="flex-end" gap={2}>
+        <Grid>
+          <Dropdown>
+            <Dropdown.Trigger>
+              <Avatar />
+            </Dropdown.Trigger>
+            <Dropdown.Menu color="secondary" aria-label="Settings">
+              <Dropdown.Section title="Transaction Settings">
+                
+              </Dropdown.Section>
+              <Dropdown.Section title="Interface Settings">
+                <div>
+                  <div>
+                    <Text h5>Auto Router API</Text>
+                    <Tooltip content="Your transaction will revert if the price changes unfavorably by more than this percentage" placement="left">
+                      
+                    </Tooltip>
+                  </div>
+                </div>
+                <div>
+
+                </div>
+              </Dropdown.Section>
+            </Dropdown.Menu>
+          </Dropdown>
+        </Grid>
+      </Grid.Container>
     </Card>
   </div>
   ]
