c395080efb13b48958f33cebc615acc31aa704ac
nhad1702 <anhducnguyenhuu2@gmail.com>
2022-07-10 10:06:51 +0700

    resolve conflict

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,52 +1,52 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { Container, Image, Pagination, Card, Grid, Dropdown, Modal, Text, Button } from "@nextui-org/react";
+import {
+  Container,
+  Image,
+  Pagination,
+  Card,
+  Grid,
+  Dropdown,
+  Modal,
+  Text,
+  Button,
+} from "@nextui-org/react";
 import UniSwapLogo from "../assets/uniswap-uni-logo.svg";
-import "./css/logo.css"
-import "./css/header.css"
+import "./css/logo.css";
+import "./css/header.css";
 
 const Header = () => {
   const [visible, setVisible] = React.useState(false);
   const handler = () => setVisible(true);
 
   const closeHandler = () => {
-    setVisible(false)
-    console.log("close")
-  }
+    setVisible(false);
+    console.log("close");
+  };
 
   return (
     <header>
       <div className="logo">
         <Link to={"/"}>
-          <Image src={UniSwapLogo} className='logo'/>
+          <Image src={UniSwapLogo} className="logo" />
         </Link>
       </div>
 
       <div className="categories">
-        <Grid.Container gap={2} justify='center'>
-            <Grid>Swap</Grid>
-            <Grid>Pool</Grid>
-            <Grid>Vote</Grid>
-            <Grid>Chart</Grid>
+        <Grid.Container gap={2} justify="center">
+          <Grid>Swap</Grid>
+          <Grid>Pool</Grid>
+          <Grid>Vote</Grid>
+          <Grid>Chart</Grid>
         </Grid.Container>
-        <Dropdown disableAnimation>
-          <Dropdown.Button flat color="primary">
-            Select token
-          </Dropdown.Button>
-          <Dropdown.Menu>Select a network</Dropdown.Menu>
-        </Dropdown>
-        <div>
-        <Button auto shadow onClick={handler}>Connect Wallet</Button>
-        <Modal closeButton open={visible} onClose={closeHandler}>
-
-        </Modal>
-        </div>
-        <Dropdown disableAnimation>
-          <Dropdown.Button flat>...</Dropdown.Button>
-
-        </Dropdown>
       </div>
-      <div className="settings"></div>
+
+      <div className="settings">
+        <Button auto shadow onClick={handler}>
+          Connect Wallet
+        </Button>
+        <Modal closeButton open={visible} onClose={closeHandler}></Modal>
+      </div>
     </header>
   );
 };
