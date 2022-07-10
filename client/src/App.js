c9513b580a782dd666a7e2b9f2332a6d3f02b6c6
NightmareDreams08 <nhan100434@gmail.com>
2022-07-10 09:15:38 +0700

    a

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,15 @@
 import { NextUIProvider } from '@nextui-org/react';
 import './App.css';
+import Vote from './pages/Vote';
+import Votev2 from './pages/Votev2';
 
 function App() {
   return (
     <NextUIProvider>
+      {/* <Vote/> */}
+      <Votev2/>
     </NextUIProvider>
+    
   );
 }
 
---
+++ client/src/components/Vote.jsx
@@ -0,0 +1,17 @@
+
+import React from 'react';
+import './App.css';
+import "../css/Vote.css"
+
+
+function Votecp() {
+  return (
+    <div className="votecp">
+       <p></p>
+       <p></p>
+       <p></p>
+  </div>
+  );
+}
+
+export default Votecp;
