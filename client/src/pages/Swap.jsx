2b480def168db9179be28c310f0409331cfeacdd
nhad1702 <anhducnguyenhuu2@gmail.com>
2022-07-10 10:59:20 +0700

    Added swap component

--- client/src/pages/Swap.jsx
+++ client/src/pages/Swap.jsx
@@ -1,38 +1,23 @@
 import React from "react";
 import {  Avatar, Card, Dropdown, Grid, Text, Tooltip } from "@nextui-org/react";
+import { FontAwesome } from 'react-icons';
 import "../components/css/swap.css";
 
 const Swap = () => {
   return [
     <div>
-    <Card className="swaptoken">
-      <Text h4>Swap</Text>
-      <Grid.Container justify="flex-end" gap={2}>
-        <Grid>
-          <Dropdown>
-            <Dropdown.Trigger>
-              <Avatar />
-            </Dropdown.Trigger>
-            <Dropdown.Menu color="secondary" aria-label="Settings">
-              <Dropdown.Section title="Transaction Settings">
-                
-              </Dropdown.Section>
-              <Dropdown.Section title="Interface Settings">
-                <Grid.Container>
-                  <Grid>
-                    <Text h5>Auto Router API</Text>
-                    <Tooltip content="Your transaction will revert if the price changes unfavorably by more than this percentage" placement="left">
-                      
-                    </Tooltip>
-                  </Grid>
-                </Grid.Container>
+    <Grid.Container justify="center">
+      <Grid>
+        <Card className="swap">
+        <Text h5>Swap</Text>
+        <Dropdown placement="">
+          <Dropdown.Button>
 
-              </Dropdown.Section>
-            </Dropdown.Menu>
-          </Dropdown>
-        </Grid>
-      </Grid.Container>
-    </Card>
+          </Dropdown.Button>
+        </Dropdown>
+        </Card>
+      </Grid>
+    </Grid.Container>
   </div>
   ]
 };
