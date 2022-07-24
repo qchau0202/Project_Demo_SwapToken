020ddd0464aab301c05d656efb9bfe2535ea17d1
NightmareDreams08 <nhan100434@gmail.com>
2022-07-24 10:38:01 +0700

    The Wallet

--- client/src/pages/Swap.jsx
+++ client/src/pages/Swap.jsx
@@ -1,35 +1,61 @@
-import React from "react";
-import { Card, Dropdown, Grid, Input, Text } from "@nextui-org/react";
-import { AiOutlineSetting, AiOutlineArrowDown } from 'react-icons/ai';
-import { WalletMultiButton } from "@solana/wallet-adapter-react-ui";
+import React, { useCallback } from "react";
+import { Card, Dropdown, Grid, Input,  Button } from "@nextui-org/react";
+import { AiOutlineSetting, AiOutlineArrowDown } from "react-icons/ai";
+import { useWallet, useConnection } from "@solana/wallet-adapter-react";
+import { Transaction, SystemProgram, LAMPORTS_PER_SOL } from "@solana/web3.js";
 import "../components/css/swap.css";
 
 export default function Swap() {
-  
-  return [
-    <div>
+  const { connection } = useConnection();
+  const { publicKey, sendTransaction } = useWallet();
+
+  const handleOnSubmit = useCallback(async (event) => {
+    event.preventDefault();
+    console.log("hsahdkjjjashkd");
+    const { recipientAddress, amount } = event.target;
+
+    if (!publicKey) {
+      alert("Please open a wallet first");
+      return;
+    }
+
+    const transaction = new Transaction().add(
+      SystemProgram.transfer({
+        fromPubkey: publicKey,
+        toPubkey: recipientAddress.value,
+        lamports: amount.value * LAMPORTS_PER_SOL,
+      })
+    );
+
+    const signature = await sendTransaction(transaction, connection);
+    await connection.confirmTransaction(signature, "processed");
+    alert ("You have successfully transferred coins")
+    recipientAddress.value = "";
+    amount.value = "";
+  }, [publicKey, sendTransaction, connection]);
+
+  return (
     <Grid.Container justify="center">
-      <Grid className="swap">
+      <form className="swap" onSubmit={handleOnSubmit}>
         <Card className="swap1">
           <div className="top">
             <h5>Swap</h5>
             <AiOutlineSetting />
           </div>
           <Card>
-            <Input label="From" placeholder="0.0" />
+            <Input label="From" placeholder="0.0" name="recipientAddress" />
             <Dropdown></Dropdown>
           </Card>
           <AiOutlineArrowDown />
           <Card>
-            <Input label="To (estimated)" placeholder="0.0" />
+            <Input label="To (estimated)" placeholder="0.0" name="amount" />
             <Dropdown></Dropdown>
           </Card>
-          <WalletMultiButton />
+          <Button className="button" auto color={`error`} type="submit">
+            Create Proposal
+          </Button>
         </Card>
-      </Grid>
+      </form>
     </Grid.Container>
-  </div>
-  ]
+  );
 }
-
-
