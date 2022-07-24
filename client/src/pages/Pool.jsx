4b3b2dfdd4092e354ee172dd9670b2e76f742a62
Chou Lee <Chou@chous-device.local>
2022-07-24 10:11:42 +0700

    icon and pool updates

--- client/src/pages/Pool.jsx
+++ client/src/pages/Pool.jsx
@@ -1,4 +1,3 @@
-import { Grid } from "@nextui-org/react";
 import React from "react";
 import "../components/css/pool.css";
 import { WalletMultiButton } from "@solana/wallet-adapter-react-ui";
@@ -10,10 +9,19 @@ const Pool = () => {
         <div className="top">
           <p>Pools Overview</p>
         </div>
-
         <div className="box">
           <p>Your active V3 liquidity positions will appear here.</p>
           <WalletMultiButton/>
+        </div>
+        <div className="bottom">
+         <div className="links-a">
+          <p>Learn about providing liquidity</p>
+          <p className="des">Check out our v3 LP walkthrough and migration guides.</p>
+         </div>
+         <div className="links-b">
+          <p>Top pools</p>
+          <p className="des">Explore Uniswap Analytics.</p>
+         </div>
         </div>
       </div>
     </>
